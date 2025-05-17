f2527441c05c3f63def7e90a7f804af531c77ac5
bodinggg <66398434+bodinggg@users.noreply.github.com>
2025-05-17 21:17:40 +0800

    Update tabel_structrue_recognizer.py

--- take_apart/utils/tabel_structrue_recognizer.py
+++ take_apart/utils/tabel_structrue_recognizer.py
@@ -21,9 +21,21 @@ from collections import Counter
 import numpy as np
 from huggingface_hub import snapshot_download
 
-from api.utils.file_utils import get_project_base_directory
-from rag.nlp import rag_tokenizer
-from .recognizer import Recognizer
+
+from nlp import rag_tokenizer
+from recognizer import Recognizer
+
+
+def get_project_base_directory():
+    return os.path.abspath(
+        os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            os.pardir,
+            os.pardir,
+        )
+    )
+
+
 
 
 class TableStructureRecognizer(Recognizer):
